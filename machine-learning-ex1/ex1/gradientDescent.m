41de0372b280e07b61611aa6d8b7bbd74d591037
hyeongwoo shin <shw5122@gmail.com>
2018-04-22 18:50:28 +0900

    Add solution

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    % Calculating h
+    h = X * theta; % X(:,1) are always 1  , 
+    % Calcualting Errors
+    errors = h - y;
+    delta = X' * errors;
+    theta = theta - (alpha / m) * delta;
+    fprintf('%i\n', theta)
 
     % ============================================================
 
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,15 +16,19 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    
+    % Calculating h
+    h = X * theta; % X(:,1) are always 1  , 
+    % Calcualting Errors
+    errors = h - y;
+    delta = X' * errors;
+    theta = theta - (alpha / m) * delta;
+    fprintf('%i\n', theta)
 
+    % ============================================================
 
-
-
-
-
-
-
-
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
 
 
     % ============================================================
